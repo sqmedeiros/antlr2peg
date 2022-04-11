87acbbd1c68afaeddda6048dfca67aede41c0305
Sergio <sqmedeiros@gmail.com>
2022-04-11 16:04:26 -0300

    New version of DOT.lua generated by ANTLR2Peg

--- grammars/dot/DOT.lua
+++ grammars/dot/DOT.lua
@@ -1,11 +1,8 @@
-local m = require 'pegparser.parser'
-local pretty = require 'pegparser.pretty'
-local coder = require 'pegparser.coder'
-local recovery = require 'pegparser.recovery'
-local ast = require'pegparser.ast'
-local util = require'pegparser.util'
-local first = require'pegparser.first'
-local cfg2peg = require'pegparser.cfg2peg'
+local Parser = require 'pegparser.parser'
+local Pretty = require 'pegparser.pretty'
+local Util = require'pegparser.util'
+local Cfg2Peg = require'pegparser.cfg2peg'
+local Coder = require'pegparser.coder'
 
 local s = [===[
 graph   <-   STRICT? (GRAPH   /  DIGRAPH ) id? '{' stmt_list '}' 
@@ -41,16 +38,17 @@ PREPROC   <-   '#' (![\r\n] .)*
 WS   <-   [ \t\n\r]+
 ]===]
 
-g = m.match(s)
-print(m.match(s))
-print(pretty.printg(g, true), '\n')
-first.calcFst(g)
-first.calcFlw(g)
-first.getChoiceReport(g)
-first.getRepReport(g)
-local p = coder.makeg(g, 'ast')
-local peg = cfg2peg.convert(g, 'ID')
-print(pretty.printg(peg, true), '\n')
-local dir = util.getPath(arg[0])
-util.testYes(dir .. '/yes/', 'dot', p)
+local g = Parser.match(s)
+assert(g)
+pretty = Pretty.new()
+print(pretty:printg(g, nil, true))
+local c2p = Cfg2Peg.new(g)
+c2p:setPredUse(false)
+c2p:convert('ID', true)
+local peg = c2p.peg
+print(pretty:printg(peg, nil, true))
+
+local p = Coder.makeg(peg)
+local dir = Util.getPath(arg[0])
+Util.testYes(dir .. '/yes/', 'dot', p)
 
